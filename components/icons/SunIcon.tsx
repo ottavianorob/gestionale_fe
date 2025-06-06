bf6a4c8b15e6d74aa15de1f14d033b844c1cd5af
ottavianorob <me@robertoottaviano.it>
2025-06-06 18:59:11 +0200

    Delete SunIcon.tsx

--- components/icons/SunIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { IconProps } from '../../types';
-
-export const SunIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-6.364-.386l1.591-1.591M3 12h2.25m.386-6.364l1.591 1.591M12 12a2.25 2.25 0 00-2.25 2.25 2.25 2.25 0 002.25 2.25c1.24 0 2.25-1.01 2.25-2.25A2.25 2.25 0 0012 12z" />
-  </svg>
-);
