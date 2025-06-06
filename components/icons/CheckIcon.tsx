79dcc63a5c99272f20c6ffae791e082cc76bc8aa
ottavianorob <me@robertoottaviano.it>
2025-06-06 18:59:53 +0200

    Delete CheckIcon.tsx

--- components/icons/CheckIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import type { IconProps } from '../../types';
-
-export const CheckIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-  </svg>
-);
