a048c77ac71c9b386ac5ab78d12ff7f1bbbc78e3
ottavianorob <me@robertoottaviano.it>
2025-06-06 18:59:56 +0200

    Delete CheckCircleIcon.tsx

--- components/icons/CheckCircleIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import type { IconProps } from '../../types';
-
-export const CheckCircleIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-);
