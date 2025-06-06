9343ef9eb2adbd50315c6a15b114cefee03db621
ottavianorob <me@robertoottaviano.it>
2025-06-06 19:00:06 +0200

    Delete ArrowLeftIcon.tsx

--- components/icons/ArrowLeftIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import type { IconProps } from '../../types';
-
-export const ArrowLeftIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-  </svg>
-);
