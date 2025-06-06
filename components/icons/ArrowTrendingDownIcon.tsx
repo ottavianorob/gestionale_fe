a833eefd8f9202a4b8c7c55f3fc635ebb03be8da
ottavianorob <me@robertoottaviano.it>
2025-06-06 19:00:04 +0200

    Delete ArrowTrendingDownIcon.tsx

--- components/icons/ArrowTrendingDownIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import type { IconProps } from '../../types';
-
-export const ArrowTrendingDownIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6 9 12.75l4.306-4.306a11.95 11.95 0 0 1 5.814 5.519l2.74 1.22m0 0-5.94 2.28m5.94-2.28L15.75 19.5M2.25 6H9m-6.75 0V12.75" />
-</svg>
-);
